b36b1e0159ac59a88f15abaa42c31a5f9518737a
kj4tmp <56497124+kj4tmp@users.noreply.github.com>
2024-04-20 17:15:48 -0700

    nb submodule

--- src/pyethercat_ext.cpp
+++ src/pyethercat_ext.cpp
@@ -5,5 +5,5 @@ namespace nb = nanobind;
 using namespace nb::literals;
 
 NB_MODULE(pyethercat_ext, m) {
-    m.def("add", [](int a, int b) { return a + b; }, "a"_a, "b"_a);
+    m.def("add", [](int a, int b) { return a + b; }, "a"_a, "b"_a, "This function adds two numbers and increments if only one is provided.");
 }
