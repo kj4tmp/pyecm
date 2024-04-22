bdcd2dc7de516498a8f4d21dd6b1729483b98ebd
kj4tmp <56497124+kj4tmp@users.noreply.github.com>
2024-04-22 06:37:48 +0000

    osal

--- src/pyethercat/soem_ext.cpp
+++ src/pyethercat/soem_ext.cpp
@@ -11,6 +11,11 @@ NB_MODULE(soem_ext, m) {
     // ethercat.h
     // nothing in here, yeet
 
+    // from osal.h
+    nb::class_<ec_timet> (m, "ec_timet")
+        .def_rw("sec", &ec_timet::sec)
+        .def_rw("usec", &ec_timet::usec);
+
     // ethercattype.h
     nb::enum_<ec_err_type> (m, "ec_err_type")
         .value("EC_ERR_TYPE_SDO_ERROR", ec_err_type::EC_ERR_TYPE_SDO_ERROR)
@@ -35,5 +40,21 @@ NB_MODULE(soem_ext, m) {
         .value("EC_STATE_ACK", ec_state::EC_STATE_ACK)
         .value("EC_STATE_ERROR", ec_state::EC_STATE_ERROR);
 
-    // convert the above ctype def into nanobind
+
+    // TODO: figure out how to do this nexted union crap
+    // nb::class_<ec_errort>(m, "ec_errort")
+    //     .def_rw("Time", &ec_errort::Time)
+    //     .def_rw("Signal", &ec_errort::Signal)
+    //     .def_rw("Slave", &ec_errort::Slave)
+    //     .def_rw("Index", &ec_errort::Index)
+    //     .def_rw("SubIdx", &ec_errort::SubIdx)
+    //     .def_rw("Etype", &ec_errort::Etype)
+    //     .def_rw("AbortCode", &ec_errort::AbortCode);
+        // .def_rw("ErrorCode", [](const ec_errort& error) -> uint16& { return error.SpecificError.ErrorCode; })
+        // .def_rw("ErrorReg", [](const ec_errort& error) -> uint8& { return error.SpecificError.ErrorReg; })
+        // .def_rw("b1", [](const ec_errort& error) -> uint8& { return error.SpecificError.b1; })
+        // .def_rw("w1", [](const ec_errort& error) -> uint16& { return error.SpecificError.w1; })
+        // .def_rw("w2", [](const ec_errort& error) -> uint16& { return error.SpecificError.w2; });
+
+
 }
